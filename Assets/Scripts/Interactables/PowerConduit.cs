1de6410a2b3162085dc12027049a037dbb5be090
Kyle Ballagher <k.ballagher@gmail.com>
2020-07-19 16:27:59 +0100

    Spawning Interactables

---
+++ Assets/Scripts/Interactables/PowerConduit.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class PowerConduit : MonoBehaviour, IInteractable
+{
+    private GameObject _highlight;
+
+    private void Awake()
+    {
+        _highlight = gameObject.transform.Find("InteractionHalo").gameObject;
+        _highlight.SetActive(false);
+    }
+
+    public void HighlightObject(bool enabled)
+    {
+        _highlight.SetActive(enabled);
+    }
+
+    public void UseObject()
+    {
+        Debug.Log("Power Conduit Used");
+        gameObject.tag = "Environment";
+        HighlightObject(false);
+    }
+}
--- Assets/Scripts/WorldObjects/InteractionSpot.cs
+++ Assets/Scripts/WorldObjects/InteractionSpot.cs
@@ -2,5 +2,22 @@
 
 public class InteractionSpot : MonoBehaviour
 {
+    private GameObject[] _interactablePrefabs;
 
+    private void Awake()
+    {
+        _interactablePrefabs = Resources.LoadAll<GameObject>("Prefabs/Interactables");
+
+        SpawnInteractable();
+
+        Destroy(gameObject);
+    }
+
+    private void SpawnInteractable()
+    {
+        var i = Random.Range(0, _interactablePrefabs.Length);
+        var selectedInteractable = _interactablePrefabs[i];
+
+        Instantiate(selectedInteractable, transform.position, transform.rotation);
+    }
 }
