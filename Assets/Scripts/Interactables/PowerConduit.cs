0b247095edf8b6b7d7db8bfba13d1a54ab70d906
Kyle Ballagher <k.ballagher@gmail.com>
2020-07-19 16:50:29 +0100

    Fixable Power Conduits

--- Assets/Scripts/Interactables/PowerConduit.cs
+++ Assets/Scripts/Interactables/PowerConduit.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 public class PowerConduit : MonoBehaviour, IInteractable
 {
     private GameObject _highlight;
+    private SpriteRenderer _leftCore;
+    private SpriteRenderer _rightCore;
 
     private void Awake()
     {
         _highlight = gameObject.transform.Find("InteractionHalo").gameObject;
         _highlight.SetActive(false);
+
+        _leftCore = gameObject.transform.Find("Conduit/LeftCore").GetComponent<SpriteRenderer>();
+        _rightCore = gameObject.transform.Find("Conduit/RightCore").GetComponent<SpriteRenderer>();
     }
 
     public void HighlightObject(bool enabled)
@@ -19,7 +24,12 @@ public class PowerConduit : MonoBehaviour, IInteractable
     public void UseObject()
     {
         Debug.Log("Power Conduit Used");
+
+        _leftCore.color = Color.green;
+        _rightCore.color = Color.green;
+
         gameObject.tag = "Environment";
+
         HighlightObject(false);
     }
 }
