d2856158ae2a52997eccc77b0ddea2fed1b85eb6
Kyle Ballagher <k.ballagher@gmail.com>
2020-07-21 16:38:35 +0100

    New Door Button Sprites

--- Assets/Scripts/WorldObjects/Door.cs
+++ Assets/Scripts/WorldObjects/Door.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.U2D;
 
 public class Door : MonoBehaviour
 {
     private Animator _anim;
+    private SpriteRenderer _wDoorPanel;
+    private SpriteRenderer _eDoorPanel;
+    private Sprite _openPanel;
+    private Sprite _closedPanel;
 
     private bool _doorState;
 
@@ -10,6 +15,15 @@ public class Door : MonoBehaviour
     {
         _anim = GetComponent<Animator>();
         _doorState = _anim.GetBool("Open");
+    }
+
+    private void Start()
+    {
+        _wDoorPanel = gameObject.transform.Find("WDoorPanel").GetComponent<SpriteRenderer>();
+        _eDoorPanel = gameObject.transform.Find("EDoorPanel").GetComponent<SpriteRenderer>();
+        Sprite[] _atlas = Resources.LoadAll<Sprite>("Sprites/Interactables/doorButtons");
+        _openPanel = _atlas[0];
+        _closedPanel = _atlas[1];
     }
 
     public void ToggleDoor()
@@ -23,6 +37,22 @@ public class Door : MonoBehaviour
         {
             _anim.SetBool("Open", false);
             _doorState = false;
+        }
+
+        SetPanelSprites();
+    }
+
+    private void SetPanelSprites()
+    {
+        if (_doorState)
+        {
+            _wDoorPanel.sprite = _openPanel;
+            _eDoorPanel.sprite = _openPanel;
+        }
+        else
+        {
+            _wDoorPanel.sprite = _closedPanel;
+            _eDoorPanel.sprite = _closedPanel;
         }
     }
 
