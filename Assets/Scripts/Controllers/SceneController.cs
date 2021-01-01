5751cb042b66d80948a5aeca00be8bd2d94ebcfb
Kyle Ballagher <k.ballagher@gmail.com>
2021-01-01 15:36:38 +0000

    Scene Controller, Fade between scenes

---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private Animator _anim;
+
+    private int _sceneIndexToLoad;
+
+    void Awake()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        SetupSingleton();
+
+        Invoke("LoadMainMenu", 5);
+    }
+
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        _anim.SetTrigger("FadeIn");
+    }
+
+    void SetupSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void LoadMainMenu()
+    {
+        FadeToScene(1);
+    }
+
+    public void FadeToScene(int index)
+    {
+        _sceneIndexToLoad = index;
+
+        _anim.SetTrigger("FadeOut");
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(_sceneIndexToLoad);
+    }
+
+    public void ResetGameScene()
+    {
+        FadeToScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
