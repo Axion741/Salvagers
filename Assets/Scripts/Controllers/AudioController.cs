b8c90c069d9477d88b43e9f0a57f902913ca4658
Kyle Ballagher <k.ballagher@gmail.com>
2021-01-01 15:03:02 +0000

    Splash/Menu Scenes, Audio Controller

---
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public float musicVolume;
+    public float sfxVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupSingleton();
+        GetMusicVolume();
+        GetSfxVolume();
+    }
+
+    void SetupSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void GetMusicVolume()
+    {
+        musicVolume = PlayerPrefs.GetFloat("musicVolume", 0.5f);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        musicVolume = volume;
+    }
+
+    void GetSfxVolume()
+    {
+        sfxVolume = PlayerPrefs.GetFloat("sfxVolume", 0.5f);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+        sfxVolume = volume;
+    }
+}
